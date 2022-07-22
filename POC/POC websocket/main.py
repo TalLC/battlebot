e82a19787f174c9612b8c1e62fa9c592de433a17
viallya <95406322+viallya@users.noreply.github.com>
2022-07-22 10:36:01 +0200

    Add files via upload

--- POC/POC websocket/main.py
+++ POC/POC websocket/main.py
@@ -1,3 +1,4 @@
+import random
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 
@@ -6,34 +7,65 @@ app = FastAPI()
 html = """
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
+	<head>
+		<meta charset="utf-8">
+		<title>My first three.js app</title>
+		<style>
+			body { margin: 0; }
+		</style>
+	</head>
+	<body>
+	<script async src="https://unpkg.com/es-module-shims@1.3.6/dist/es-module-shims.js"></script>
+    <script type="importmap">
+    {
+    "imports":  {
+                    "three": "https://unpkg.com/three@0.142.0/build/three.module.js"
+                }
+    }
+    </script>
+    <script type="module">
+            import * as THREE from 'three';
             var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
+            
+            
+  		    console.log("Script lancer")
+			const scene = new THREE.Scene();
+			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+			const renderer = new THREE.WebGLRenderer();
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			document.body.appendChild( renderer.domElement );
+
+			const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+			const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+			const cube = new THREE.Mesh( geometry, material );
+			scene.add( cube );
+
+			camera.position.z = 5;
+
+			function animate(dep) {
+				requestAnimationFrame( animate );
+				var cha = dep.split(":");
+                var val = parseFloat(cha[1]);
+                if (cha[0] == 'x'){
+				    cube.rotation.x += val;
+				}
+				else if (cha[0] == 'y'){
+				    cube.rotation.y += val;
+				}
+				else{
+				    cube.rotation.z += val;
+                }
+				renderer.render( scene, camera );
+			};
+			
+			ws.onmessage = function(event) {
+                var dep = event.data;
+                animate(dep);
             };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
+
+	</script>
+	</body>
 </html>
 """
 
@@ -47,5 +79,14 @@ async def get():
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+        rand = random.randint(1, 5)
+        if rand == 1:
+            await websocket.send_text("x:0.5")
+        elif rand == 2:
+            await websocket.send_text("y:1")
+        elif rand == 3:
+            await websocket.send_text("z:2.5")
+        elif rand == 4:
+            await websocket.send_text("x:2.5")
+        elif rand == 5:
+            await websocket.send_text("y:1.5")
