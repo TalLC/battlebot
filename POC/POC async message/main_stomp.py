f9872e84f765e73a45a226ff2864b0d5bc2d5540
Bob74 <olivierfouchecourt@gmail.com>
2022-08-14 16:16:05 +0200

    Typage des fonctions + envoi des données au format json

--- POC/POC async message/main_stomp.py
+++ POC/POC async message/main_stomp.py
@@ -1,37 +1,44 @@
-import sys
 import stomp
 import logging
+import json
 from time import sleep
 
-logging.basicConfig(level=logging.DEBUG)
 
-
-def connect_stomp(up, hp) -> stomp.Connection:
-    conn = stomp.Connection(host_and_ports=hp)
-    conn.connect(up, up, wait=True)
-
+def connect_stomp(username=None, passcode=None, host: str = "localhost", port: int = 61613) -> stomp.Connection:
+    conn = stomp.Connection(host_and_ports=[(host, port)])
+    conn.connect(username=username, passcode=passcode, wait=True)
     return conn
 
 
-def send_message(connexion, message):
-    connexion.send(body='<message>compteur=%s<message>'.format(message), destination='stomptest')
+def send_message(connection: stomp.Connection, destination: str, message: dict):
+    connection.send(destination=destination, body=json.dumps(message))
 
 
-def receive_message(connexion):
-    connexion.subscribe('stomptest', 123)
+def receive_message(connection: stomp.Connection):
+    connection.subscribe(destination='stomptest', id=str(123))
 
 
 if __name__ == '__main__':
+    # logging
+    logging.basicConfig(level=logging.DEBUG, datefmt='%d/%m/%Y %I:%M:%S',
+                        format='[%(levelname)s] %(asctime)s - %(message)s')
+
     # settings
-    userpass = "admin"
-    hap = [("localhost", 61613)]
-    count = 0
+    user = "user"
+    pwd = "user"
+    host = "localhost"
+    port = 61613
+
+    queue = "stomptest"
 
     # connect to broker
-    stomp_co = connect_stomp(userpass, hap)
+    stomp_co = connect_stomp(user, pwd, host, port)
+    if not stomp_co.is_connected():
+        raise "Failed to connect to broker"
 
     # publisher
+    count = 0
     while True:
-        send_message(stomp_co, f"compteur = {count}")
+        send_message(stomp_co, queue, {'compteur': count})
         sleep(3)
         count += 1
