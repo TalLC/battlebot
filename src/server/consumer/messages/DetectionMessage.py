8827384d88cf8700ea23c0488ed86efdfdc71c5a
Bob74 <olivierfouchecourt@gmail.com>
2022-09-02 15:07:09 +0200

    Ajout de classes pour les messages à envoyer aux bots

--- src/server/consumer/messages/DetectionMessage.py
+++
@@ -1,8 +0,0 @@
-from consumer.messages.interfaces.IScannerMessage import IScannerMessage
-
-
-class DetectionMessage(IScannerMessage):
-    _INFO_TYPE = "object_detection"
-
-    def __init__(self, bot_id: str, objects: list):
-        super().__init__(bot_id, self._INFO_TYPE, objects)
