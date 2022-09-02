4a01da5a77d34a612577f51a4b4ed462afa6d436
Bob74 <olivierfouchecourt@gmail.com>
2022-09-02 19:02:36 +0200

    Séparation de RestAPI en 3 classes Rest, Website et Websocket parties Producer et Consumer

--- src/server/consumer/brokers/MQTTConsumer.py
+++ src/server/consumer/brokers/MQTTConsumer.py
@@ -1,26 +1,26 @@
-from consumer.interfaces.IBrokerConsumer import IBrokerConsumer
-from utils import stomp
+from consumer.brokers.interfaces.IBrokerConsumer import IBrokerConsumer
+from utils import mqtt
 
 
-class STOMPConsumer(IBrokerConsumer):
+class MQTTConsumer(IBrokerConsumer):
 
     def __init__(self):
-        self.__client = stomp.get()
+        self.__client = mqtt.get()
 
     def get_destination(self) -> str:
         """
         Return the root path of the topics.
         """
-        return stomp.get().destination_root
+        return mqtt.get().destination_root
 
     def _send_message(self, destination: str, message: dict, retain: bool = False):
         """
         Send a message to a topic.
         """
-        self.__client.send_message(topic=destination, message=message)
+        self.__client.send_message(topic=destination, message=message, retain=retain)
 
     def close(self):
         """
-        Close the STOMP client.
+        Close the MQTT client.
         """
         self.__client.close()
--- src/server/consumer/brokers/STOMPConsumer.py
+++ src/server/consumer/brokers/STOMPConsumer.py
@@ -1,26 +1,26 @@
-from consumer.interfaces.IBrokerConsumer import IBrokerConsumer
-from utils import mqtt
+from consumer.brokers.interfaces.IBrokerConsumer import IBrokerConsumer
+from utils import stomp
 
 
-class MQTTConsumer(IBrokerConsumer):
+class STOMPConsumer(IBrokerConsumer):
 
     def __init__(self):
-        self.__client = mqtt.get()
+        self.__client = stomp.get()
 
     def get_destination(self) -> str:
         """
         Return the root path of the topics.
         """
-        return mqtt.get().destination_root
+        return stomp.get().destination_root
 
     def _send_message(self, destination: str, message: dict, retain: bool = False):
         """
         Send a message to a topic.
         """
-        self.__client.send_message(topic=destination, message=message, retain=retain)
+        self.__client.send_message(topic=destination, message=message)
 
     def close(self):
         """
-        Close the MQTT client.
+        Close the STOMP client.
         """
         self.__client.close()
