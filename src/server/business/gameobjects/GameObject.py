d2d52887b017dc6d806d6d5d248ca732417052c4
Bob74 <olivierfouchecourt@gmail.com>
2022-09-21 22:02:48 +0200

    Renommage de 'heading' en 'ry' + passage des coordonnées en float + suppr de la fonction deploy

--- src/server/business/gameobjects/GameObject.py
+++ src/server/business/gameobjects/GameObject.py
@@ -1,12 +1,10 @@
 
-"""
-    Base class for every objects on the map.
-"""
-
 
 class GameObject:
-
-    def __init__(self, name: str = 'game_object', x: int = 0, z: int = 0):
+    """
+    Base class for every objects on the map.
+    """
+    def __init__(self, name: str = 'game_object', x: float = 0.0, z: float = 0.0):
         self.name = name
         self.x = x
         self.z = z
