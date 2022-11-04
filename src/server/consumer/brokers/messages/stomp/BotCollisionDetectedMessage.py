6b73c6277390a357b52e46408195d0e816c6a7f9
Bob74 <olivierfouchecourt@gmail.com>
2022-11-04 12:11:10 +0100

    Fix de l'héritage

--- src/server/consumer/brokers/messages/stomp/BotCollisionDetectedMessage.py
+++ src/server/consumer/brokers/messages/stomp/BotCollisionDetectedMessage.py
@@ -1,7 +1,7 @@
-from consumer.brokers.messages.stomp.interfaces.IBotCollisionMessage import IBotCollisionMessage
+from consumer.brokers.messages.stomp.interfaces.IBotStatMessage import IBotStatMessage
 
 
-class BotCollisionDetectedMessage(IBotCollisionMessage):
+class BotCollisionDetectedMessage(IBotStatMessage):
 
     def __init__(self, bot_id: str, object_name: str):
         super().__init__(bot_id=bot_id, msg_type="collision_detected", stat_value=object_name)
