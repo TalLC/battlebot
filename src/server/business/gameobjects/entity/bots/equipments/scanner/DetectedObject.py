cd66d45545891f9f74f85c31d36d8d62843c2210
P-e-ach <95888680+P-e-ach@users.noreply.github.com>
2023-01-13 11:55:59 +0100

    Ajout d'un id aux DetectedObject

--- src/server/business/gameobjects/entity/bots/equipments/scanner/DetectedObject.py
+++ src/server/business/gameobjects/entity/bots/equipments/scanner/DetectedObject.py
@@ -1,7 +1,8 @@
 
 class DetectedObject:
 
-    def __init__(self, name: str | None, a_from: float, a_to: float, distance: float):
+    def __init__(self, obj_id: str, name: str | None, a_from: float, a_to: float, distance: float):
+        self.id = obj_id
         self.name = name
         self.a_from = a_from
         self.a_to = a_to
