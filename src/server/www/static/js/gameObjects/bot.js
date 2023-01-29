c55e8c995be75621483ee427dfe96b9ae702b8fa
Bob74 <olivierfouchecourt@gmail.com>
2023-01-29 21:50:51 +0100

    typo

--- src/server/www/static/js/gameObjects/bot.js
+++ src/server/www/static/js/gameObjects/bot.js
@@ -5,8 +5,8 @@ import {colorStrToNumber} from '../utils.js'
 
 
 export default class Bot extends GameObject{
-    constructor(id, x, z, ry, teamColor, collisionShape, collisionShize, modelName) {
-        super(id, "bot", x, 0.5, z, ry,  collisionShape, collisionShize);
+    constructor(id, x, z, ry, teamColor, collisionShape, collisionSize, modelName) {
+        super(id, "bot", x, 0.5, z, ry,  collisionShape, collisionSize);
         this.teamColor = colorStrToNumber(teamColor);
         this.modelName = modelName;
         this.sceneObject = null;
