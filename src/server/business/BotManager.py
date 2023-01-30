36f5d010a5e67d35155f87c7fe90891f8a35bd75
Bob74 <olivierfouchecourt@gmail.com>
2023-01-30 23:00:52 +0100

    Ajout d'une fonction pour retrouver les bots d'un rayon donné

--- src/server/business/BotManager.py
+++ src/server/business/BotManager.py
@@ -1,3 +1,4 @@
+from business.shapes.ShapesUtils import ShapesUtils
 from business.interfaces.IBotManager import IBotManager
 from business.gameobjects.entity.bots.models.BotModel import BotModel
 from business.gameobjects.entity.bots.BotFactory import BotFactory
@@ -34,6 +35,24 @@ class BotManager(IBotManager):
             bots = (bot for bot in self._BOTS.values() if bot.client_connection.is_connected is connected_only)
 
         return tuple(bots)
+
+    def get_bots_in_radius(self, connected_only: bool = True,origin: tuple = (0.0, 0.0),
+                           radius: int = 1) -> (BotModel,):
+        """
+        Get all connected bots.
+        """
+        bots = tuple(self._BOTS.values())
+
+        if connected_only:
+            bots = (bot for bot in self._BOTS.values() if bot.client_connection.is_connected is connected_only)
+
+        # Filtering on radius
+        in_range_bots = list()
+        for bot in bots:
+            if ShapesUtils.get_2d_distance_between(origin, (bot.x, bot.z)) <= radius:
+                in_range_bots.append(bot)
+
+        return tuple(in_range_bots)
 
     def create_bot(self, bot_name, bot_type) -> BotModel:
         """
