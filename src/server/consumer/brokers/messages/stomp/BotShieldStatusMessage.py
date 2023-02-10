ab5fcb88c0b8c2d46941bbb6a567fd6c6e7df5ea
Bob74 <olivierfouchecourt@gmail.com>
2023-02-10 12:12:22 +0100

    Clean des classes non utilisées

--- src/server/consumer/brokers/messages/stomp/BotShieldStatusMessage.py
+++
@@ -1,7 +0,0 @@
-from consumer.brokers.messages.stomp.interfaces.IBotStatMessage import IBotStatMessage
-
-
-class BotShieldStatusMessage(IBotStatMessage):
-
-    def __init__(self, bot_id: str, durability_percent: int):
-        super().__init__(bot_id=bot_id, msg_type="shield_status", stat_value=durability_percent)
