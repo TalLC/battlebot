41e21011c46062c3d0be9043b044314145cab5c8
Bob74 <olivierfouchecourt@gmail.com>
2023-02-17 17:07:05 +0100

    Ajout du lancement de activeMQ

--- src/server/start-server.bat
+++ src/server/start-server.bat
@@ -1,3 +1,9 @@
 @echo off
+echo Lancement des brokers ActiveMQ
+start cmd /k activemq\bin\activemq.bat start
+
+echo Laisser le temps à ActiveMQ de démarrer (quelques secondes)
+timeout /t 10
+
 echo Lancement du serveur
 .\venv\Scripts\python.exe -m uvicorn main:app --port 8000 --reload --timeout-keep-alive 30
