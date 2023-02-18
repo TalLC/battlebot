d995e10f20ef482e84b534b393d0c3f78d8298e5
Bob74 <olivierfouchecourt@gmail.com>
2023-02-18 03:20:52 +0100

    Ajout du packaging de la lib client

--- src/make-package.bat
+++ src/make-package.bat
@@ -23,9 +23,14 @@ rmdir /S /Q _package\activemq\data\tmp
 rmdir /S /Q _package\venv
 del /Q _package\*.bak
 
-rem Zip du package
+rem Zip du package serveur
 cd _package
 ..\7za.exe a -tzip -r ..\battlebots-server-package *
+cd ..
+
+rem Zip du package de la lib client
+cd battlebotslib-sources
+..\7za.exe a -tzip -r ..\battlebots-client-lib *
 cd ..
 
 pause
