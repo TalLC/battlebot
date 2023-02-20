973ec3817c4d69cdff6a404b1f8aae5db4366a9f
Bob74 <olivierfouchecourt@gmail.com>
2023-02-20 19:15:06 +0100

    Utilisation du ping comme sleep car problème avec timeout + définition de JAVA_HOME sur le java portable

--- src/server/start-server.bat
+++ src/server/start-server.bat
@@ -2,11 +2,17 @@
 cls
 chcp 65001 >nul
 rem ---------------------------------
+rem utilisation du Java portable
+set JAVA_HOME="%cd%\third-party\jre1.8.0"
+
+rem Lancement de ActiveMQ
 echo Lancement des brokers ActiveMQ
-start cmd /k activemq\bin\activemq.bat start
+start cmd /C activemq\bin\activemq.bat start
 
+rem Attente de 10 secondes
 echo Laisser le temps à ActiveMQ de démarrer (quelques secondes)
-timeout /t 10
+ping 127.0.0.1 -n 10 > nul
 
+rem Lancement du serveur Battlebots
 echo Lancement du serveur
 venv\Scripts\python.exe -m uvicorn main:app --port 8000 --reload --timeout-keep-alive 30
