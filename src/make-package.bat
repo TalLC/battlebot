b34988361ab567a3a756d0c0083baa51d8cda7b5
Bob74 <olivierfouchecourt@gmail.com>
2023-02-20 19:14:21 +0100

    Téléchargement de Python et java portables

--- src/make-package.bat
+++ src/make-package.bat
@@ -11,8 +11,47 @@ rem Partie serveur
 rem --------------
 rem Création du dossier de serveur temporaire
 set tmp_server=_tmp_server
+set third_party=server\third-party
+rem Python
+set python_package=Winpython64-3.10.9.0dot
+set python_package_dir=WPy64-31090\python-3.10.9.amd64
+set python_final_dir=python3.10
+rem Java
+set java_package=jre-8u361-windows-x64
+set java_package_dir=jre1.8.0_361
+set java_final_dir=jre1.8.0
+
 rmdir /S /Q %tmp_server%
+mkdir %third_party%
 mkdir %tmp_server%
+
+rem Téléchargement de Java JRE 1.8
+echo Check de la présence de Java...
+if not exist %third_party%\%java_final_dir%\ (
+    echo Téléchargement de Java...
+    powershell.exe -Command "Invoke-WebRequest -UserAgent 'Wget' -Uri 'https://sourceforge.net/projects/portableapps/files/Java/%java_package%.tar.gz/download' -OutFile '%third_party%\%java_package%.tar.gz';"
+    cd %third_party%
+    ..\..\7za.exe x "%java_package%.tar.gz" -o* -y
+    ..\..\7za.exe x "%java_package%.tar" -o* -y
+    move "%java_package%\%java_package_dir%" "%java_final_dir%"
+    rmdir /S /Q "%java_package%.tar"
+    rmdir /S /Q "%java_package%"
+    del /Q "%java_package%.tar.gz"
+    cd ..\..
+)
+
+rem Téléchargement de Python
+echo Check de la présence de Python...
+if not exist %third_party%\%python_final_dir%\ (
+    echo Téléchargement de Python...
+    powershell.exe -Command "Invoke-WebRequest -Uri 'https://github.com/winpython/winpython/releases/download/5.3.20221233/%python_package%.exe' -OutFile '%third_party%\%python_package%.exe';
+    cd %third_party%
+    "%python_package%.exe" -o* -y
+    move "%python_package%\%python_package_dir%" "%python_final_dir%"
+    rmdir /S /Q "%python_package%"
+    del /Q "%python_package%.exe"
+    cd ..\..
+)
 
 rem Recopie du code Python Serveur
 robocopy /E server %tmp_server%
