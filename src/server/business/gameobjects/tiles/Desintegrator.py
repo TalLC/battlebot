662928bf4edfb4a9a887efed183a7248b5b32a72
P-e-ach <95888680+P-e-ach@users.noreply.github.com>
2023-02-24 11:52:02 +0100

    Ajout de la tuile "desintegrator"

---
+++ src/server/business/gameobjects/tiles/Desintegrator.py
@@ -0,0 +1,16 @@
+from business.gameobjects.tiles.Tile import Tile
+from business.gameobjects.tiles.objects.TileObject import TileObject
+from business.gameobjects.tiles.objects.Air import Air
+
+
+class Desintegrator(Tile):
+    _NAME: str = "Desintegrator"
+
+    @property
+    def is_walkable(self) -> bool:
+        return False
+
+    def __init__(self, x: int, z: int, tile_object: TileObject = None):
+        super().__init__(
+            name=self._NAME, x=x, z=z, tile_object=Air(parent_tile=self) if tile_object is None else tile_object
+        )
