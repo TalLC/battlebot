66790866f836b65e1f3333e9554f6bd43beb294d
Bob_74 <olivierfouchecourt@gmail.com>
2023-03-30 10:08:57 +0200

    Fix des chemins avec des espaces

--- server/start-server.bat
+++ server/start-server.bat
@@ -6,11 +6,11 @@ rem Suppression du mode debug
 venv\Scripts\python.exe third-party\set-debug.py debug=false
 
 rem utilisation du Java portable
-set JAVA_HOME="%cd%\third-party\jre1.8.0"
+set JAVA_HOME=%cd%\third-party\jre1.8.0
 
 rem Lancement de ActiveMQ
 echo Lancement des brokers ActiveMQ
-start cmd /C %cd%\third-party\activemq\bin\activemq.bat start
+start cmd /C "%cd%\third-party\activemq\bin\activemq.bat" start
 
 rem Attente de 10 secondes
 echo Laisser le temps à ActiveMQ de démarrer (quelques secondes)
@@ -19,3 +19,4 @@ ping 127.0.0.1 -n 10 > nul
 rem Lancement du serveur Battlebots
 echo Lancement du serveur
 venv\Scripts\python.exe -m uvicorn main:app --port 8000 --reload --timeout-keep-alive 30
+pause
