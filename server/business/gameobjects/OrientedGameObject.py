7b1276efa48faf22c6df7964145f7ed84a30046b
Bob74 <olivierfouchecourt@gmail.com>
2023-05-13 01:27:20 +0200

    Ajout des vecteurs face et arrière

--- server/business/gameobjects/OrientedGameObject.py
+++ server/business/gameobjects/OrientedGameObject.py
@@ -1,3 +1,4 @@
+import math
 from math import pi
 from business.gameobjects.GameObject import GameObject
 from shapely.geometry import Polygon
@@ -22,6 +23,14 @@ class OrientedGameObject(GameObject):
         Sets the value to a value between 0.0 and 2pi.
         """
         self._ry = value % (2*pi)
+
+    @property
+    def forward_vector(self) -> tuple:
+        return math.sin(self.ry), math.cos(self.ry)
+
+    @property
+    def backward_vector(self) -> tuple:
+        return math.sin(-1 * self.ry), math.cos(-1 * self.ry)
 
     def __init__(self, name: str = 'oriented_game_object', friendly_name: str = 'Oriented game object',
                  object_type: str = 'object', ry: float = 0.0, x: float = 0.0, z: float = 0.0,
